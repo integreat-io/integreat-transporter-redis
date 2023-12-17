f7c419cbe5f757efc174ab50ed59bab7fdf2cb78
Kjell-Morten <post@kjellmorten.no>
2023-12-17 23:06:41 +0100

    Don't rerun tests on changes in Redis

--- ava.config.cjs
+++ ava.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   extensions: { ts: 'module' },
   nodeArguments: ['--loader=ts-node/esm', '--no-warnings'],
-  ignoredByWatcher: ['{coverage,dist,media}/**', '**/*.md'],
+  ignoredByWatcher: ['{coverage,dist,media}/**', '**/*.md', 'redisdata/**'],
   files: ['src/**/*.test.ts'],
 }
